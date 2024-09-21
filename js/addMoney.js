a07db3c335df451c83bf938c5b736b263d7e6a29
Abid0624 <abidkhan09989@gmail.com>
2024-09-22 02:12:42 +0600

    full project done

--- js/addMoney.js
+++ js/addMoney.js
@@ -15,11 +15,25 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 //   console.log('add money value', addMoney);
   const addMoney = getInputFieldValueById('input-add-money');
   const pinNumber = getInputFieldValueById('input-pin-number');
+
+  if(isNaN(addMoney)){
+    alert('Failed to add money');
+    return;
+  }
   
   if(pinNumber === 1234){
     const balance = getTextFieldValueById('account-balance');
     const newBalance = balance + addMoney;
     document.getElementById('account-balance').innerText = newBalance;
+
+    // add to transaction history
+    const p = document.createElement('p');
+    p.innerText = `Added : ${addMoney} Tk. Balance: ${newBalance}`;
+    console.log(p);
+    // 
+
+    // show the p in the transaction
+    document.getElementById("transaction-container").appendChild(p);
   }
   else{
     alert('Failed to Add Money!Please try again later')
